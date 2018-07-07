782d720601c3e5da1ce5f29151409578d7ef4ae4
Christian Aguayo <caguayo@elektra.com.mx>
2018-07-06 19:13:35 -0500

    últimos commits de Filebrains

---
+++ Shortcuts.java
@@ -0,0 +1,9 @@
+
+
+  Java Programming for Android Developers for Dummies
+'https://drive.google.com/uc?export=download&id=0B2t-Tmujl-IbSXR1VENkSWFzZG8'
+  Descargado: EBooks!
+
+  Los mejores libros de Desarrollo en .pdf
+'http://www.allitebooks.in'
+  Descargado: EBooks!
